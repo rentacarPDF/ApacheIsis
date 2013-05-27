44259750a7242fd11e47a55a2ebc05a4fd68c8d1
silvana noemi figueroa <sil-figueroa@hotmail.com>
2013-05-26 22:15:56 -0300

    modificacion Fixture

--- myapp/myapp-ProbandoMenu/src/main/java/org/myapp/ProbandoMenu/MenuNuevo.java
+++ myapp/myapp-ProbandoMenu/src/main/java/org/myapp/ProbandoMenu/MenuNuevo.java
@@ -21,11 +21,6 @@ import dom.todo.ToDoItems;
 
 @Named("MenuNuevo")
 
-// muchachines bueno para que les agregue el menu siempre tiene que tener un submenu
-//asi solito no te deja porque.... nose..eso lo que estoy viendo ahora. por ahora le subo esto
-// bueno aca utilice las clase AbstractService que estan en fixture por lo que si le dan f3 a AbstractService
-//van a encontrar un par de metodos(get y set) que es solo para el nombre del menu ... pero despues ahi otra clase
-//abstracta que es la de AbstractFixture que si vamos a ToDoItemsFixture nos paramos sobr ella hacemos F3 nos llevara ahi.. que es lo que estoy analizando ahora.
 
 public class MenuNuevo extends AbstractService
 {
